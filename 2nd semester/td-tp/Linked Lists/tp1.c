c173e32fcc274833cd94869157d3d9fd02cbe1ba
Billelrm <126027378+Billelrm@users.noreply.github.com>
2023-02-28 00:03:53 +0100

    sizeof() function
    Exercice 01 (TP)
    Écrire un programme en C qui utilise la fonction sizeof et qui permet d’afficher la taille des types de données : int,
    char, float, long, double, long double.

---
+++ 2nd semester/td-tp/Linked Lists/tp1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(){
+printf("%d\n %d\n %d\n %d\n %d\n %d\n ",sizeof(int),sizeof(char),sizeof(float),sizeof(long),sizeof(double),sizeof(long double));   
+   return 0;
+}
