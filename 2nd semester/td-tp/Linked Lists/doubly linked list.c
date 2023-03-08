925a8d75e0beca824e7c937803a4798b676e74a0
Billelrm <126027378+Billelrm@users.noreply.github.com>
2023-03-08 18:23:17 +0100

    creating a doubly linked list

--- 2nd semester/td-tp/Linked Lists/doubly linked list.c
+++ 2nd semester/td-tp/Linked Lists/doubly linked list.c
@@ -49,16 +49,8 @@ if (*head==NULL) {
               ptr->next=NewElement;
         }
      }
-     
-     void deleteFirst(struct node** head){
-         if (*head==NULL) printf("the list is empty");
-         else { struct node* ptr=*head;
-         (*head)=(*head)->next;
-          free(ptr);
-          (*head)->previous=NULL; }
-         }
 
-      void deleteFirst2(struct node** head){
+      void deleteFirst(struct node** head){
         if (*head==NULL) printf("the list is empty");
          else {
          (*head)=(*head)->next;
@@ -66,7 +58,17 @@ if (*head==NULL) {
           (*head)->previous=NULL; }
       }   
 
-      void deleteLast(struct node* head){ 
+       struct node* deleteFirst2(struct node* head){
+        if (head==NULL) printf("the list is empty");
+        else { struct node* ptr=head;
+           head=head->next;
+           free(ptr);
+           head->previous=NULL;
+        }               
+          return head;
+         }
+
+      void deleteLast(struct node* head){ //not sure if it works or not//
         if (head==NULL) printf("the list is empty");
         else { 
            struct node* ptr=head;
@@ -122,6 +124,54 @@ if (*head==NULL) {
               return count;
             }
           }
+
+          struct node* deleteNode(struct node* head,int pos){ //this function is for deleting a node in any position//
+          int lastnode=count(head);
+          int firstnode=1;
+
+              if (pos==firstnode) return deleteFirst2(head);
+              if (pos==lastnode){
+                  deleteLast(head);
+                return head;  }
+            
+              if (pos!=firstnode && pos!=lastnode){
+                struct node* ptr=head;
+                while (pos!=1){
+                    ptr=ptr->next;
+                    pos--;
+                }
+                ptr->previous->next=ptr->next;
+                ptr->next->previous=ptr->previous;
+                 free(ptr);
+                 ptr=NULL;
+              }
+               
+               return head;
+             }
+          
+          struct node* addNode(struct node* head,int x, int pos){ //this function is for adding a node after a given position
+          int lastnode=count(head);
+          if (pos==lastnode){
+            add_at_end(head,x);
+            return head;
+            }
+          if (pos!=lastnode){
+            struct node* ptr=head;
+            struct node* newN=malloc(sizeof(struct node*));
+            newN->data=x;
+            newN->previous=NULL;
+            newN->next=NULL;
+            while (pos!=1){
+                ptr=ptr->next;
+                pos--;
+                }
+             newN->previous=ptr;
+             newN->next=ptr->next;
+             ptr->next->previous=newN;
+             ptr->next=newN;   
+          }
+             return head;
+          }
        
 
 
@@ -129,11 +179,10 @@ int main() {
     struct node* head=NULL;
     int choice=8;
     int x;
-    add_at_beg(&head,2);
-    add_at_end(head,3);
     while (choice!=0){
         printf("\n");
-  display(head);
+
+
   printf("\t 1 for adding a node in the beginning \n");
   printf("\t 2 for adding a node in the end \n");
   printf("\t 3 for seeing if the element exists or not \n");
@@ -142,6 +191,8 @@ int main() {
   printf("\t 6 for counting how many nodes exist \n");
   printf("\t 7 for counting how many time x exists \n");
   printf("\t 8 for displaying the list \n");
+  printf("\t 9 for deleting a node in any position \n");
+  printf("\t 10 for adding a node after a given position \n");
   printf("\t 0 for ending the program \n");
   scanf("%d",&choice);
   system("clear");  
@@ -164,7 +215,7 @@ int main() {
 	else printf("\n %d exists in the list",x);
   	break; 
  case 4:
-   deleteFirst2(&head);
+   deleteFirst(&head);
    printf("first node deleted\n");
     break;
  case 5:
@@ -181,12 +232,22 @@ int main() {
     break;
  case 8:
     display(head);
+    break;
+ case 9: 
+    int position;
+     printf("\n enter the the position of the node you want to delete");
+     scanf("%d",&position);
+     head=deleteNode(head,position);
+     break;
+  case 10:
+  int position1,x;
+  printf("\n enter the position and the value ");
+  scanf("%d %d",&position1,&x);
+  head=addNode(head,x,position1);
+     break;
  case 0:
  	break;
  	}  		
     }
-
-   
-
-    return 0;
-}
+     return 0;
+    }
